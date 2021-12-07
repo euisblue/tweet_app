dfab6762557e7008e2f2c20ec4b709e5b005ed9a
Blue Eu <euisblue@pm.me>
2021-12-08 08:17:30 +0900

    post details page complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'posts/index' => "posts#index"
+  get 'posts/:id' => "posts#show"
+
+  get '/' => "home#top"
+  get 'top' => "home#top"
+  get 'about' => "home#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
