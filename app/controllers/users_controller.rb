a20b7c64c8bf6cd873be152d555f851344d4fcca
Blue Eu <euisblue@pm.me>
2021-12-16 08:09:48 +0900

    user authentication/login/edit/delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user, {only: [:index, :show, :edit, :update, :destroy]}
+  before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
+  before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
+
+  def ensure_correct_user 
+    if @current_user.id != params[:id].to_i 
+      flash[:notice] = "Unauthorized access"
+      redirect_to("/posts/index")
+    end
+  end
+
   def index
     @users = User.all
   end
-  
+
   def show 
     @user = User.find_by(id: params[:id])
   end
@@ -15,9 +26,11 @@ class UsersController < ApplicationController
     @user = User.new(
       name: params[:name], 
       email: params[:email],
-      image_name: "default.jpg"
+      image_name: "default.jpg",
+      password: params[:password]
     )
     if @user.save 
+      session[:user_id] = @user.id
       flash[:notice] = "You have signed up successfully"
       redirect_to("/users/#{@user.id}")
     else 
@@ -33,6 +46,7 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     @user.name = params[:name]
     @user.email = params[:email]
+    @user.password = params[:password]
 
     if params[:image]
       @user.image_name = "#{@user.id}.jpg" 
@@ -47,4 +61,38 @@ class UsersController < ApplicationController
       render("users/edit")
     end
   end 
+
+  def login_form
+  end
+
+  def login 
+    @user = User.find_by(
+      email: params[:email], 
+      password: params[:password]
+    )
+    if @user 
+      session[:user_id] = @user.id
+      flash[:notice] = "You have logged in successfully"
+      redirect_to("/posts/index")
+    else 
+      @error_message = "Invalid email/password combination"
+      @email = params[:email]
+      @password = params[:password]
+      render("users/login_form")
+    end
+  end
+
+  def logout 
+    session[:user_id] = nil
+    flash[:notice] = "You have logged out successfully"
+    redirect_to("/login")
+  end
+
+  def destroy 
+    @user = User.find_by(id: params[:id])
+    flash[:notice] = "Bye, '#{@user.name}'!"
+    @user.destroy
+    session[:user_id] = nil
+    redirect_to("/")
+  end
 end
