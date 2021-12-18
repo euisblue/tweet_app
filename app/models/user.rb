a5d5d5ce36dd0314644c237046bde3a1824e427f
Blue Eu <euisblue@pm.me>
2021-12-18 17:59:18 +0900

    secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :password, {presence: true}
+  has_secure_password
+
   validates :name, {presence: true}
   validates :email, {presence: true, uniqueness: true}
 
