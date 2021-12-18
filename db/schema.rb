0fa3a265b9c641e54072205467fa4ce1a4f33466
Blue Eu <euisblue@pm.me>
2021-12-18 20:33:37 +0900

    db migrated to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_12_18_064000) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
